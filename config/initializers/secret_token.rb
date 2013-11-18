345c05e5f59a3b96f9db3db19eee4f777eedb031
Ryan Riley <Ryan.B.Riley@colorado.edu>
2013-11-18 08:06:24 -0700

    Initializing Repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EngineeringProject::Application.config.secret_key_base = '75cd53bd4a15d2cba0dcce90df28b0d7fa58c623ff8ffec923249708aad631d95ad9405339f806018862e50182d07f9f4e57f079d2cdbb86976873b5fa6fbf38'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EngineeringProject::Application.config.session_store :cookie_store, key: '_engineeringProject_session'
