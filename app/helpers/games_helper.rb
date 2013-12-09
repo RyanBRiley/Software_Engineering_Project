085e672cf7863db512c532df87a8a0d8945a2b7d
Ryan Riley <Ryan.B.Riley@colorado.edu>
2013-12-08 22:36:37 -0700

    adding large icons for games/show

--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -3,4 +3,9 @@ module GamesHelper
     size = options[:size]
     image_tag(asset_path("#{game.id}.jpg"), alt: "", class: "icon")
   end
+
+ def big_icon_for(game, options = { size: 25 })
+    size = options[:size]
+    image_tag(asset_path("#{game.id}main.jpg"), alt: "", class: "icon")
+  end
 end
