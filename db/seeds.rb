a02a14f546945b1d8a082967523a2ef006578dec
Ryan Riley <Ryan.B.Riley@colorado.edu>
2013-12-19 15:20:02 -0700

    adding other games

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,3 +8,21 @@
 
 
 
+
+lol = Game.create!(game_name: "League of Legends",
+                         game_id: 2,
+                         user_id: 2,
+                         info: "A MOBA that has taken over the e-sport scene.")
+
+dota2 = Game.create!(game_name: "DoTA 2",
+                         game_id: 3,
+                         user_id: 3,
+                         info: "The sequel to the MOBA that started it all.")
+
+sc2 = Game.create!(game_name: "Starcraft 2",
+                         game_id: 4,
+                         user_id: 4,
+                         info: "The most popular RTS ever, huge in the e-sport community of Korea.")
+
+
+
